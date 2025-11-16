13e8eaa01c77524484a0f50363ee8ff274c1fda6
azgaurvittiwari6-cmy <azgaurvittiwari6@gmail.com>
2025-11-16 13:58:30 +0530

    first commit

--- day1.js
+++ day1.js
@@ -6,3 +6,24 @@ let a = 23
 let b = 24
 let c = a+b 
 console.log(c);
+
+let e = 34
+let f = 25
+let d = a-b 
+console.log(d);
+
+let g = 45
+let h = 38
+let i = g/h
+console.log(i);
+
+let j = 89
+let k = 98
+let l = j*k
+console.log(l);
+
+
+
+
+
+
---
+++ day2.js
@@ -0,0 +1,23 @@
+// Learn about prompt
+
+let ch = prompt("Enter a  character")
+console.log(ch);
+
+let num =  Number = (prompt("Enter a number "))
+console.log(num)
+
+if(ch>=18){
+    console.log("You can give vote.");
+    
+}else{
+    console.log("You cannot give vote ");
+    
+}
+
+if(num>=18){
+    console.log("You are not a child.You are known as adult");
+    
+}else{
+    console.log("You are child. You are not a adule");
+    
+}
