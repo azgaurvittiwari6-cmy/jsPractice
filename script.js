ee579309b8279d6efc406e13da0acd6c737e82df
azgaurvittiwari6-cmy <azgaurvittiwari6@gmail.com>
2025-11-16 10:08:35 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let ch = prompt("Enter a character")
+console.log(ch);
+let ah = prompt("Enter a character")
+console.log(ch);
+
+if(ch>18){
+    console.log("You can take the Lenovo laptop");
+
+}else{
+    console.log("You cannot take the laptop.You will take DELL laptop");
+
+}
+
+
+for(var i = 0;i<4;i++){
+    console.log("i is the table of "+i++);
+    
+}
+
+for (let i = 1; i <= 5; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " is even");
+    } else {
+        console.log(i + " is odd");
+    }
+}
